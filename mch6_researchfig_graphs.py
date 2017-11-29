add888427c06b769c330a0a172ecbdb0dfd39bbe
Jaime McCutcheon <jem64@le.ac.uk>
2017-11-29 17:04:25 +0000

    Changed colours, tidied fig

--- mch6_researchfig_graphs.py
+++ mch6_researchfig_graphs.py
@@ -21,16 +21,26 @@ firstlick, distractedArray = jmf.distractedOrNot(lone_distractors, x.lickData['l
 
 # Use mastersnipper to make snips of distracted and non-distracted
 
-snips_distracted = jmf.mastersnipper(x, lone_distractors)
+snips = jmf.mastersnipper(x, lone_distractors, preTrial=3, trialLength=10)
 
-fig, ax = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True, figsize=(3.2, 2.4))
+fig, ax = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True, figsize=(5.8, 3.5))
 
-jmfig.trialsMultShadedFig(ax[0], [x.siUVTrials[x.distractedArray],
-                               x.siTrials[x.distractedArray]],
+ax[0].axis('off')
+ax[1].axis('off')
+
+jmfig.trialsMultShadedFig(ax[0], [snips['uv'][distractedArray],
+                               snips['blue'][distractedArray]],
                           x.pps,
-                          eventText = '')
+                          eventText = '',
+                          linecolor=['xkcd:silver', 'blue'], errorcolor=['xkcd:light grey', 'xkcd:light blue'])
 
-jmfig.trialsMultShadedFig(ax[1], [x.siUVTrials[~x.distractedArray],
-                               x.siTrials[~x.distractedArray]],
+jmfig.trialsMultShadedFig(ax[1], [snips['uv'][~distractedArray],
+                               snips['blue'][~distractedArray]],
                           x.pps,
-                          eventText = '')
+                          eventText = '',
+                          linecolor=['xkcd:silver', 'xkcd:charcoal'], errorcolor=['xkcd:light grey', 'xkcd:grey'])
+
+ax[0].plot([0,0], [0.02, 0.04], c='k')
+ax[0].text(-10, 0.03, '2% \u0394F', verticalalignment='center', horizontalalignment='right')
+
+plt.savefig('R:\\DA_and_Reward\\jem64\\1705_MCH6\\output\\photo-dist.eps')
