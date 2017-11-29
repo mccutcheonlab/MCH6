264e4689b016db178eda6411d45dea704bfe1f2c
Jaime McCutcheon <jem64@le.ac.uk>
2017-11-29 10:09:52 +0000

    Research Day file

---
+++ mch6_researchfig_graphs.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 10:07:52 2017
+
+For making figs for research day - needs to have mch6-distAnalysis run first
+
+@author: James Rig
+"""
+
+x = rats['mch6.5'].sessions['s16']
